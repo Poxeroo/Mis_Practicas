b6f3f08e69a40f52f035c72ea33ba979a22c8219
Bran Alejandro Estrella Infante <49102354+Poxeroo@users.noreply.github.com>
2023-07-24 17:57:28 -0400

    INICIO

---
+++ practicas/src/app/login/page.js
@@ -0,0 +1,11 @@
+
+export default function login() {
+    return (
+        <section className=" bg-white ">
+            <div className=" bg-blue-700 w-1/2 justify-start">
+                
+            </div>
+        </section>
+      
+      );
+  }
